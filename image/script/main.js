f9aeff32fb99c76d7b64333b95ca245040641a41
iyassen <71711072+iyassen@users.noreply.github.com>
2022-03-19 10:00:34 +0900

    Update main.js

--- image/script/main.js
+++ image/script/main.js
@@ -3,3 +3,15 @@ const date = new Date();
 const locale = date.toLocaleString();
 
 document.querySelector('#middle').innerHTML = `${locale}`;
+
+const ghorst = document.getElementById('');
+
+// 画像を透明にする処理
+const ghorstHide = () => {
+  ghorst.style.opacity = 0; // 画像要素のopacityプロパティの値を0(透明)にする
+};
+
+// 画像を不透明にする処理
+const ghorstAppear = () => {
+  ghorst.style.opacity = 1; // 画像要素のopacityプロパティの値を1(不透明)にする
+};
